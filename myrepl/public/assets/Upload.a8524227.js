2da0d90531837b6a30fc3a20503ae9a22d5478e6
SinzMise <mail@sinzmise.cf>
2023-11-16 00:54:55 +0000

    Update memos

--- myrepl/public/assets/Upload.a8524227.js
+++
@@ -1,1 +0,0 @@
-import{h as o}from"./index.c2ccb594.js";import{n as a}from"./index.caf06f59.js";import{T as e}from"./Tasks.822bdd7f.js";import"./Paginator.acedef0c.js";import"./index.2927060c.js";const n=()=>(a("manage.sidemenu.upload"),o(e,{type:"upload"}));export{n as default};
